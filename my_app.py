ce1a9be015bb436f6a5e0e52757c01cb1de5106c
Nikorambus <72225007+Nikorambus@users.noreply.github.com>
2023-05-04 15:16:49 +0300

    add_db commit

--- my_app.py
+++ my_app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:secret@192.168.31.10/delivery_service"
+db.init_app(app)
 
 
-if __name__ == "__main__":
-    app.run()
+class Tariff(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(64), nullable=False)
+    Max_mass = db.Column(db.String(64), nullable=False)
+    Time_of_delivery = db.Column(db.String(64), nullable=False)
+
+
+class Delivery(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Address = db.Column(db.String(64), nullable=False)
+    Destination = db.Column(db.String(64), nullable=False)
+    Number_of_destination = db.Column(db.String(64), nullable=False)
+    Time_of_destination = db.Column(db.String(64), nullable=False)
+    Number_of_car = db.Column(db.String(64), nullable=False)
+    Courier = db.Column(db.String(64), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
